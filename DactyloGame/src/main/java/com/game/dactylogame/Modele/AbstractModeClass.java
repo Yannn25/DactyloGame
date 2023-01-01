1df98a37e48a6363ef1477129d9a901700b3a695
Yann EDIKEU <yann.edikeu@etu.u-paris.fr>
2023-01-01 10:46:55 +0100

    ajout modif

---
+++ DactyloGame/src/main/java/com/game/dactylogame/Modele/AbstractModeClass.java
@@ -0,0 +1,72 @@
+package com.game.dactylogame.Modele;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Timer;
+import org.fxmisc.richtext.*;
+
+
+/**
+ * L'interface nous servira a regrouper toutes les attributs et mthodes qui
+ * sont commun a nos trois mode de Jeu
+ */
+public abstract class AbstractModeClass {
+
+    private int KeyPress;
+    private  Timer time;
+    private  Timer reg; //(Régularité)
+    private Tampon tampon;
+
+    /**
+     *
+     * @param str un texte
+     * @return une Liste chaine contenant tous les mots compris dans str
+     */
+    public static LinkedList<String> parse(String str) {
+        LinkedList<String> ret = new LinkedList<String>();
+        int i = 0, debM = 0, finM = 0;
+        while(i < str.length() && finM < str.length()) {
+            if(str.charAt(finM) == ' '){
+                ret.add(str.substring(debM, finM));
+                finM++;
+                debM = finM;
+                i++;
+            } else {
+                finM++;
+                i++;
+            }
+        }
+        ret.add(str.substring(debM, finM));
+        return ret;
+    }
+
+    public int getKeyPress() {
+        return KeyPress;
+    }
+    public void setKeyPress(int keyPress) {
+        KeyPress = keyPress;
+    }
+    public Timer getTime() {
+        return time;
+    }
+    public void setTime(Timer time) {
+        this.time = time;
+    }
+    public Timer getReg() {
+        return reg;
+    }
+    public void setReg(Timer reg) {
+        this.reg = reg;
+    }
+    public Tampon getTampon() {
+        return tampon;
+    }
+
+    public void RemplirTampon() {
+        Scanner scanner = new Scanner(getClass().getResourceAsStream("/com/game/dactylogame/ENListeMots.txt"));
+        while (scanner.hasNextLine()) {
+            tampon.getAllWords().add(scanner.nextLine());
+        }
+        scanner.close();
+    }
+}
---
+++ DactyloGame/src/main/java/com/game/dactylogame/VueFX/HomeController.java
@@ -0,0 +1,25 @@
+package com.game.dactylogame.VueFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeController {
+    private Stage stage;
+    private Scene scene;
+    //private Parent parent;
+    @FXML
+    private void onNormalButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/game/dactylogame/NormalMode.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,800,600);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
